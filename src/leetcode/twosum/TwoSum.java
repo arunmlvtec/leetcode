5934399b6543af9bfb1c09e7c15db676e7d72816
Arun Pokharna <arun.pokharna@gmail.com>
2017-03-29 18:55:32 -0700

    1.   Two Sum leetcode

--- src/leetcode/twosum/TwoSum.java
+++ src/leetcode/twosum/TwoSum.java
@@ -1,6 +1,7 @@
-/*
+/**
  * Leetcode Problem Solutions
  * Problem No: 1
+ * Problem name: Two Sum
  * Description: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  *              You may assume that each input would have exactly one solution, and you may not use the same element twice.
  *              Example:	Given nums = [2, 7, 11, 15], target = 9,
